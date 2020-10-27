190f9bf25d3c14e9c576005d9022f42e712e16e1
sophie-apples <sophieannemurray@gmail.com>
2020-10-27 11:12:20 +0000

    initial commit

---
+++ mysql-from-python.py
@@ -0,0 +1,15 @@
+import os
+import pymysql
+
+username = os.getenv('USER')
+
+connection = pymysql.connect(host='localhost', user=username, password='', db='Chinook')
+
+try:
+    with connection.cursor() as cursor:
+        sql= "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    connection.close()
